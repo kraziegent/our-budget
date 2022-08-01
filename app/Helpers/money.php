0304924c6324d1c5da451af809b2bddd020d6f43
Peter Olumolu <olumolupeter@gmail.com>
2022-08-02 00:15:47 +0100

    update money helpers

--- app/Helpers/money.php
+++ app/Helpers/money.php
@@ -3,12 +3,16 @@
 use Money\Money;
 use Money\Parser;
 use Money\Currency;
+use Money\Exchange;
+use Money\Converter;
 use Money\Currencies\ISOCurrencies;
+use Money\Formatter\DecimalMoneyFormatter;
 
 if (! function_exists('currencies')) {
     /**
      * Get all available currencies
      *
+     * @return array
      */
     function currencies()
     {
@@ -16,7 +20,11 @@ if (! function_exists('currencies')) {
             $moneyCurrencies = new ISOCurrencies();
 
             foreach($moneyCurrencies as $currency) {
-                $currencies[$currency->getCode()] = $moneyCurrencies->subunitFor($currency);
+                $currencies[$currency->getCode()] = [
+                    //'currency' =>
+                    'subunit' => $moneyCurrencies->subunitFor($currency),
+                    'numericcode' => $moneyCurrencies->numericCodeFor($currency)
+                ];
             }
 
             return $currencies;
@@ -45,5 +53,52 @@ if (! function_exists('makeMoney')) {
         ]);
 
         return $parser->parse(number_format($amount, $currencies->subunitFor($currency), thousands_separator: ''), $currency);
+    }
+}
+
+if (! function_exists('format')) {
+    /**
+     * Get a string representation of the Money Object.
+     *
+     * @param \Money\Money $money
+     * @return string
+     */
+    function formatMoney(Money $money)
+    {
+        $currencies = new ISOCurrencies();
+        $formatter = new DecimalMoneyFormatter($currencies);
+
+        return $formatter->format($money);
+    }
+}
+
+
+if (! function_exists('convertMoney')) {
+    /**
+     * Convert an amount in one currency to another currency using a specific rate,
+     *
+     * @param mixed $money
+     * @param string $fromCurrency
+     * @param string $toCurrency
+     * @param mixed $rate
+     * @return \Money\Money
+     */
+    function convertMoney(mixed $money, string $fromCurrency, string $toCurrency, mixed $rate)
+    {
+        if (! ($money instanceof Money)) {
+            $money = makeMoney($money, $fromCurrency);
+        }
+
+        $currencies = new ISOCurrencies();
+        $exchange = new Exchange\ReversedCurrenciesExchange(new Exchange\FixedExchange([
+            $fromCurrency => [
+                $toCurrency => (string) $rate
+            ],
+        ]));
+
+        $converter = new Converter($currencies, $exchange);
+        $toCurrency = $money->getCurrency()->getCode() === strtoupper($toCurrency) ? $fromCurrency: $toCurrency;
+
+        return $converter->convert($money, new Currency($toCurrency));
     }
 }
